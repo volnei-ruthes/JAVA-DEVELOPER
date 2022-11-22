1e99cf3bdd0b0c4726d01a47e5ffe745568e8e6b
Pichau <Pichau@DESKTOP-PDAC7Q0>
2022-11-21 21:19:39 -0300

    teste2

--- firstjava/src/classone/firstjava/primeiraclasse.java
+++ firstjava/src/classone/firstjava/primeiraclasse.java
@@ -3,7 +3,7 @@ package classone.firstjava;
 public class primeiraclasse {
 	
 	public static void main(String[] args) {
-		System.out.println("Ola mundo! hellow!");
+		System.out.println("Ola mundo! hellow!!!");
 	}
 
 }
